7cebef41fa3b4f7e20e32e46ead495a554e5bd24
Danimarcon <danimarcondani@gmail.com>
2024-08-20 21:52:17 -0300

    Atualização dos estudos com tipos variaveis e concatenação

---
+++ tipos-variaveis/src/edu/daniela/operadores/Operadores.java
@@ -0,0 +1,9 @@
+package edu.daniela.operadores;
+
+public class Operadores {
+
+    public static void main (String[] args) {
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    System.out.println(nomeCompleto);
+    }
+}
---
+++ tipos-variaveis/src/edu/daniela/operadores/concatenacao.java
@@ -0,0 +1,29 @@
+package edu.daniela.operadores;
+
+public class concatenacao {
+
+    public static void main (String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+    }
+}
